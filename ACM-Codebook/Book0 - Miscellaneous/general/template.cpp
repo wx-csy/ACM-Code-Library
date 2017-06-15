56f636b11cd8566d709c3df44a3bc7c8687f8594
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2017-06-15 21:59:59 +0800

    update codebook

--- ACM-Codebook/Book0 - Miscellaneous/general/template.cpp
+++ ACM-Codebook/Book0 - Miscellaneous/general/template.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <stack>
 #include <queue>
+#include <string>
 #include <algorithm>
 using namespace std;
 
---
+++ ACM-Codebook/Book0 - Miscellaneous/string/aho-corasick.cpp
@@ -0,0 +1,50 @@
+struct AC : Trie{
+    int fail[MAXN];
+    int last[MAXN];
+
+    void construct(){
+        queue<int> q;
+        fail[0] = 0;
+        rep (c, CHARN){
+            if (int u = tr[0][c]){
+                fail[u] = 0;
+                q.push(u);
+                last[u] = 0;
+            }
+        }
+        while (!q.empty()){
+            int r = q.front(); q.pop();
+            rep (c, CHARN){
+                int u = tr[r][c];
+                if (!u){
+                    tr[r][c] = tr[fail[r]][c];
+                    continue;
+                }
+                q.push(u);
+                int v = fail[r];
+                while (v && !tr[v][c]) v = fail[v];
+                fail[u] = tr[v][c];
+                last[u] = tag[fail[u]] ? fail[u] : last[fail[u]];
+            }
+        }
+    }
+
+    void found(int pos, int j){
+        if (j) {
+            // ! add codes for having found word with tag[j]
+            found(pos, last[j]);
+        }
+    }
+
+    void find(const char* text){
+        int p = 0, c, len = strlen(text);
+        rep (i, len){
+            c = id(text[i]);
+            p = tr[p][c];
+            if (tag[p])
+                found(i, p);
+            else if (last[p])
+                found(i, last[p]);
+        }
+    }
+};
