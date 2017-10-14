8eb2006c99fc0b3369d21e16921b3b55cfa905c8
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2017-10-14 23:53:00 +0800

    minor changes

--- ACM-Codebook/Book1 - Graph Theory/flow/dinic.cpp
+++ ACM-Codebook/Book1 - Graph Theory/flow/dinic.cpp
@@ -26,11 +26,11 @@ struct Dinic {
         q.push(s);
         vis[s] = 1;
         d[s] = 0;
-        while(!q.empty()) {
+        while (!q.empty()) {
             int x = q.front(); q.pop();
-            for(int i = 0; i < G[x].size(); i++) {
+            for (int i = 0; i < G[x].size(); i++) {
                 edge& e = edges[G[x][i]];
-                if(!vis[e.to] && e.cap > e.flow) {
+                if (!vis[e.to] && e.cap > e.flow) {
                     vis[e.to] = 1;
                     d[e.to] = d[x] + 1;
                     q.push(e.to);
@@ -41,9 +41,9 @@ struct Dinic {
     }
 
     LL dfs(int x, LL a) {
-        if(x == t || a == 0) return a;
+        if (x == t || a == 0) return a;
         LL flow = 0, f;
-        for(int& i = cur[x]; i < G[x].size(); i++) {
+        for (int& i = cur[x]; i < G[x].size(); i++) {
             edge& e = edges[G[x][i]];
             if(d[x] + 1 == d[e.to] && (f = dfs(e.to, min(a, e.cap-e.flow))) > 0) {
                 e.flow += f;
@@ -59,16 +59,16 @@ struct Dinic {
     LL max_flow(int s, int t) {
         this->s = s; this->t = t;
         LL flow = 0;
-        while(bfs()) {
+        while (bfs()) {
             memset(cur, 0, sizeof(cur));
-            flow += dfs(s, INT_MAX);
+            flow += dfs(s, LLONG_MAX);
         }
         return flow;
     }
 
     vector<int> min_cut() { // call this after maxflow
         vector<int> ans;
-        for(int i = 0; i < edges.size(); i++) {
+        for (int i = 0; i < edges.size(); i++) {
             edge& e = edges[i];
             if(vis[e.from] && !vis[e.to] && e.cap > 0) ans.push_back(i);
         }
