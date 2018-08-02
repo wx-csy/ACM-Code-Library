7bdae85829307f025e2e4ddf8d88f7babc5da727
Shaoyuan CHEN <sy_chen@smail.nju.edu.cn>
2018-08-02 22:05:46 +0800

    change pell's equation and cleanup

--- ACM-Codebook/Book2 - Number Theory/combine/permutation.cpp
+++ ACM-Codebook/Book2 - Number Theory/combine/permutation.cpp
@@ -3,19 +3,15 @@ typedef vector<int> perm;
 perm operator * (const perm lhs, const perm rhs){
     int sz;
     assert((sz = lhs.size()) == rhs.size());
-    perm res; res.resize(sz);
-    for (int i=0; i<sz; i++){
-        res[i] = rhs[lhs[i]];
-    }
+    perm res(sz);
+    rep (i, sz) res[i] = rhs[lhs[i]];
     return res;
 }
 
 perm operator ~ (const perm lhs){
     int sz = lhs.size();
-    perm res; res.resize(sz);
-    for (int i=0; i<sz; i++){
-        res[lhs[i]] = i;
-    }
+    perm res(sz);
+    rep (i, sz) res[lhs[i]] = i;
     return res;
 }
 
@@ -26,7 +22,7 @@ struct permutation{
     permutation(perm p){
         size = p.size();
         vector<bool> visited(size);
-        for (int i=0; i<size; i++){
+        rep (i, size) {
             if (visited[i]) continue;
             int cur = i;
             vector<int> orbit;
