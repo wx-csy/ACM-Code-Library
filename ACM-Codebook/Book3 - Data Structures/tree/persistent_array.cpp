4f2458655e08c9d59c38a68d505ba82ae1d38d92
wx-csy <sy_chen@smail.nju.edu.cn>
2018-08-18 23:37:37 +0800

    add berlekamp-massey and persistent array

--- ACM-Codebook/Book3 - Data Structures/tree/persistent_array.cpp
+++ ACM-Codebook/Book3 - Data Structures/tree/persistent_array.cpp
@@ -1,57 +1,67 @@
 struct node {
+  static int n, pos;
+
   union {
     int value;
     struct {
       node *left, *right;
     };
   };
-
+  
   void* operator new(size_t size);
 
-
-  node *update(int l, int r, int pos, int val) const {
+  static node* build(int l, int r, int* il) {
     node* a = new node;
     if (r > l + 1) {
       int mid = (l + r) / 2;
-      if (pos < mid) {
-        a->left = left->update(l, mid, pos, val);
-        a->right = right;
-      } else {
-        a->left = left;
-        a->right = right->update(mid, r, pos, val);
-      }
+      a->left = build(l, mid, il);
+      a->right = build(mid, r, il);
+    } else {
+      a->value = il[l];
+    }
+    return a;
+  }
+
+  static node* init(int size, int* il) {
+    n = size;
+    pos = 0;
+    return build(0, n, il);
+  }
+
+  node *Update(int l, int r, int pos, int val) const {
+    node* a = new node(*this);
+    if (r > l + 1) {
+      int mid = (l + r) / 2;
+      if (pos < mid)
+        a->left = left->Update(l, mid, pos, val);
+      else
+        a->right = right->Update(mid, r, pos, val); 
     } else {
       a->value = val;
     }
     return a;
   }
 
-  int access(int l, int r, int pos) const {
+  int Access(int l, int r, int pos) const {
     if (r > l + 1) {
       int mid = (l + r) / 2;
-      if (pos < mid) return left->access(l, mid, pos);
-      else return right->access(mid, r, pos);
+      if (pos < mid) return left->Access(l, mid, pos);
+      else return right->Access(mid, r, pos);
     } else {
       return value;
     }
   }
+
+  int access(int index) {
+    return Access(0, n, index);
+  }
+
+  node *update(int index, int val) {
+    return Update(0, n, index, val);
+  }
 } nodes[30000000];
 
-int pos = 0;
+int node::n, node::pos;
 inline void* node::operator new(size_t size) {
   return nodes + (pos++);
 }
-
-int init[1 << 21];
-static node* build(int l, int r) {
-  node* a = new node;
-  if (r > l + 1) {
-    int mid = (l + r) / 2;
-    a->left = build(l, mid);
-    a->right = build(mid, r);
-  } else {
-    a->value = init[l];
-  }
-  return a;
-}
-
